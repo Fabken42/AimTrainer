ba55476c279ba6fe7481ba86e3808bc716b49568
Fabricio <fabken42@gmail.com>
2023-03-01 22:22:27 -0300

    v2

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
+//ADICIONAR SOM AO ACERTAR ALVO, QUANDO ALVO SOME E GAME OVER
+
 var link = document.createElement('link');
 link.setAttribute('rel', 'stylesheet');
 link.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Montserrat&family=Orbitron:wght@500&family=Poppins:wght@500;700;900&display=swap');
 document.head.appendChild(link);
-
+//---------------------------------------------------------//
 const RAIO_MAXIMO = 22
-var vidas = 4
-var tiros = 0
-var acertos = 0
-var precisao = 0
-var y_texto=35
-var x_texto=30
+const x_texto = 5
 
-const canvas = document.querySelector('canvas')
+const recordeEl = document.querySelector('.recorde')
+const pontuacaoEl = document.querySelector('.pontuacao')
+const precisaoEl = document.querySelector('.precisao')
+const btnJogarEl = document.querySelector('.btn_jogar')
+
+const menu = document.querySelector('.gameover')
+const canvas = document.querySelector('.canvas')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-
 var c = canvas.getContext('2d')
+
+//---------------------------------------------------------//
 
 function Alvo(x, y, raio) {
     this.x = x
@@ -24,26 +28,8 @@ function Alvo(x, y, raio) {
     this.aumenta = true
 
     this.atualiza = () => {
-        this.perdeVida()
         this.alteraTamanho()
         this.desenha()
-    }
-
-    this.perdeVida = () => {
-        if (this.raio < 1) {
-            vidas -= 1
-            alvos.shift()
-
-            if(vidas==0){
-                window.alert('FIM DE JOGO!')
-                window.alert(`Precisão: ${precisao.toFixed(2)}% | Acertos: ${acertos}`)
-                alvos=[]
-                acertos=0
-                tiros=0
-                precisao=0
-                velocidadeDeJogo=1000
-            }
-        }
     }
 
     this.alteraTamanho = () => {
@@ -51,8 +37,12 @@ function Alvo(x, y, raio) {
             this.aumenta = false
 
         if (this.aumenta)
-            this.raio += 0.2
-        else this.raio -= 0.2
+            this.raio += .2
+        else this.raio -= .2
+
+        if (this.raio < 1) {
+            perdeVida()
+        }
     }
 
     this.desenha = () => {
@@ -77,25 +67,62 @@ var mouse = {
     x: 0,
     y: 0
 }
+//------------------------FUNÇÕES-------------------------------
 
-var alvos = []
-velocidadeDeJogo = 1000
-let timer = null
+function inicializaVariveis() {
+    alvos = []
+    velocidadeDeJogo = 1000
+    timer = null
+    vidas = 4
+    tiros = 0
+    acertos = 0
+    precisao = 0
+    y_texto = Math.floor(canvas.width / 38)
+}
 
 function adicionaAlvo() {
+    mouse.x = 0
+    mouse.y = 0
+
     clearInterval(timer)
     timer = setInterval(() => {
         adicionaAlvo()
     }, velocidadeDeJogo)
-    let x = Math.floor(Math.random() * (canvas.width - RAIO_MAXIMO * 2) + RAIO_MAXIMO)
-    let y = Math.floor(Math.random() * (canvas.height - RAIO_MAXIMO * 2 - y_texto) + RAIO_MAXIMO + y_texto)
-    mouse.x = 0
-    mouse.y = 0
+    let x = Math.floor(Math.random() * (canvas.width - (RAIO_MAXIMO * 4)) + (RAIO_MAXIMO * 2))
+    let y = Math.floor(Math.random() * (canvas.height - (RAIO_MAXIMO * 4)) + (RAIO_MAXIMO * 2) + (y_texto / 2))
     alvos.push(new Alvo(x, y, 1))
 }
 
+function perdeVida() {
+    vidas -= 1
+    alvos.shift()
+
+    if (!vidas) {
+        fimDeJogo()
+    }
+}
+
+function fimDeJogo() {
+    clearInterval(timer)
+    canvas.classList.add('hide-canvas')
+    menu.classList.remove('hide-gameover')
+
+    var recorde = getRecordeLocal()
+    if (recorde < acertos)
+        localStorage.setItem('recorde', acertos)
+
+    recordeEl.textContent = `${getRecordeLocal()}`
+    pontuacaoEl.textContent = `${acertos}`
+    precisaoEl.textContent = `${precisao.toFixed(2)}%`
+}
+
+function getRecordeLocal() {
+    return parseInt(localStorage.getItem('recorde'));
+}
+
+
 function alteraVelocidade(velocidade) {
-    return velocidade > 400 ? velocidade - .12 : velocidade
+    return velocidade > 500 ? velocidade - .12 : velocidade
 }
 
 function acertaAlvo(alvos) {
@@ -108,28 +135,41 @@ function acertaAlvo(alvos) {
     return false
 }
 
-function animaJogo() { //desenhar grades brancas | limitar spawn dos alvos 
-    console.log(vidas);
+function animaJogo() {
     c.clearRect(0, 0, canvas.width, canvas.height)
     c.fillStyle = '#001624'
     c.fillRect(0, 0, canvas.width, canvas.height)
-    
-    c.fillStyle='white'
-    c.font = '30px Orbitron'
-    c.fillText(`Vidas: ${vidas} | Acertos: ${acertos} | Precisão: ${precisao.toFixed(2)}% | Velocidade: ${(1/(velocidadeDeJogo/1000)).toFixed(2)} alvos/s`,x_texto, y_texto)
-    
-    for (let i = 0; i < alvos.length; i++) {
-        alvos[i].atualiza()
-    }
-    velocidadeDeJogo = alteraVelocidade(velocidadeDeJogo)
 
-    requestAnimationFrame(animaJogo)
+    c.fillStyle = 'white'
+    c.font = `${y_texto}px Orbitron`
+    c.fillText(`Vidas: ${vidas} | Acertos: ${acertos} | Precisão: ${precisao.toFixed(2)}% | Velocidade: ${(1 / (velocidadeDeJogo / 1000)).toFixed(2)} alvos/s`, x_texto, y_texto)
+
+    alvos.forEach((alvo) => alvo.atualiza())
+
+    velocidadeDeJogo = alteraVelocidade(velocidadeDeJogo)
+    vidas ? requestAnimationFrame(animaJogo) : cancelAnimationFrame(animaJogo)
 }
+//---------------------EVENT LISTENERS------------------------
+window.addEventListener("load", () => {
+    recordeEl.textContent = `${getRecordeLocal()}`
+    pontuacaoEl.textContent = `0`
+    precisaoEl.textContent = `0`
+});
+
+btnJogarEl.addEventListener('click', () => {
+    canvas.classList.remove('hide-canvas')
+    menu.classList.add('hide-gameover')
+
+    inicializaVariveis()
+    animaJogo()
+    adicionaAlvo()
+})
 
 window.addEventListener('resize', () => {
     alvos = []
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
+    y_texto = Math.floor(canvas.width / 38)
 })
 
 canvas.addEventListener('click', (ev) => {
@@ -140,6 +180,3 @@ canvas.addEventListener('click', (ev) => {
         acertos++
     precisao = 100 * (acertos / tiros) //em porcentagem
 })
-
-adicionaAlvo()
-animaJogo()
